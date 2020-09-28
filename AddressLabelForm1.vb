1c8cae892f6e91790c434a029a57060858df26ae
harmdavi <harmondrum@gmail.com>
2020-09-27 22:03:23 -0600

    Cleaned up Code and added URL and Comments

--- AddressLabelForm1.vb
+++ AddressLabelForm1.vb
@@ -3,38 +3,21 @@
 'RCET0265
 'Fall 2020
 'Address label program
-'
+'https://github.com/harmdavi/AddressLabelForm1.git
 
 
 Public Class AddressLabelForm1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub CityTextBox_Click(sender As Object, e As EventArgs) Handles CityTextBox.Click
-
-    End Sub
-
-    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles MalingAddressGroupBox.Enter
-
-    End Sub
-
+    'This closes the app if exit button is pressed
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
-
+    'This concatenates all of the text boxes and formats them in the way that a letter would be formatted. 
     Private Sub DisplayLabelButton_Click(sender As Object, e As EventArgs) Handles DisplayLabelButton.Click
         AddressLabel.Text = FirstNameBox.Text & LastNameBox.Text _
                             & vbNewLine & StreetAddressBox.Text _
                             & vbNewLine & CityBox.Text & ", " & StateBox.Text & " " & ZipCodeBox.Text
-
-
     End Sub
-
-    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles StreetAddressBox.TextChanged
-
-    End Sub
-
+    'This resets all of the boxes if pressed
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         FirstNameBox.Text = ""
         LastNameBox.Text = ""
